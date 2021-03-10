d3757acdb8d448ccd18051e2adbcb52432765f28
Jamie Kerr <jamiestephenkerr@gmail.com>
2021-03-10 12:47:27 +0000

    Fixed script to add spss files to instrument package

--- scripts/AddSpssFilesToInstrument.ps1
+++ scripts/AddSpssFilesToInstrument.ps1
@@ -8,39 +8,43 @@ If ([string]::IsNullOrEmpty($instrumentName)) {
     throw [System.IO.ArgumentException] "No instrument name argument provided"
 }
 
-If (-not (Test-Path "$instrumentName.$env:PackageExtension")) {
-    throw [System.IO.FileNotFoundException] "$instrumentName.$env:PackageExtension file not found"
+$instrumentPackage = "$instrumentName.zip"
+
+If (-not (Test-Path $instrumentPackage)) {
+    throw [System.IO.FileNotFoundException] "$instrumentPackage file not found"
 }
 
-# Cannot extract from a zip archive without a .ZIP extension
-$instrumentPackage = "$instrumentName.$env:PackageExtension"
-
 # Create temporary folder to extract the package
-$tempPath = [guid]::NewGuid()
+$tempPath = Get-Date -format "yyyyMMddHHmmss"
 
 # Extract the instrument package into the temporary folder
 Expand-Archive $instrumentPackage -DestinationPath $tempPath
+Write-Host "Extracting instrument package to path '$tempPath'"
 
 # Download manipula package from CGP bucket
-$manipulaPackage = "SpssPackage.zip"
-gsutil cp gs://$env:ENV_BLAISE_NIFI_BUCKET/$manipulaPackage $manipulaPackage  
+$manipulaPackage = "C:\blaise\SpssPackage.zip"
+
+If (-not (Test-Path $manipulaPackage)) {
+    throw [System.IO.FileNotFoundException] "$manipulaPackage file not found"
+}
 
 # Extract the manipula package into the temporary folder
 Expand-Archive $manipulaPackage -DestinationPath $tempPath
+Write-Host "Extracting manipula package to path '$tempPath'"
 
 # Generate SPS file
-& .\$tempPath\Manipula.exe "$tempPath\GenerateStatisticalScript.msux" -K:meta="$instrumentName.bmix" -H:"" -L:"" -N:oScript="$instrumentName,iFNames=,iData=$instrumentName.bdix" -P:"SPSS;;;;;;$instrumentName.asc;;;2;;64;;Y" | Out-Null
+& cmd.exe /c .\$tempPath\Manipula.exe "$tempPath\GenerateStatisticalScript.msux" -K:meta="$instrumentName.bmix" -H:"" -L:"" -N:oScript="$instrumentName,iFNames=,iData=$instrumentName.bdix" -P:"SPSS;;;;;;$instrumentName.asc;;;2;;64;;Y" -Q:True | Out-Null
+Write-Host "Generated the .SPS file"
 
 # Generate .ASC file
-& .\$tempPath\Manipula.exe "$tempPath\ExportData_$instrumentName.msux" -A:True | Out-Null
+& cmd.exe /c .\$tempPath\Manipula.exe "$tempPath\ExportData_$instrumentName.msux" -A:True -Q:True | Out-Null
+Write-Host "Generated the .ASC file"
 
 # Add the SPS, ASC & FPS files to the instrument package
 Compress-Archive -Path "$tempPath\*.sps","$tempPath\*.asc","$tempPath\*.fps" -Update -DestinationPath $instrumentPackage
-
-# Remove the manipula package
-Remove-Item $manipulaPackage
+Write-Host "Added the SPS, ASC & FPS files to the instrument package '$instrumentPackage'"
 
 # Remove the temporary files
 Remove-Item $tempPath -Recurse
-
+Write-Host "Removed the temporary folder '$tempPath'"
 
--- scripts/DeliverInstruments.ps1
+++ scripts/DeliverInstruments.ps1
@@ -2,45 +2,51 @@
 # Data delivery pipeline script
 ###############################
 
-# If a serverpark is specified then limit the call to that server park
-$catiInstrumentsUri = if([string]::IsNullOrEmpty($env:ServerParkName)) {"$env:ENV_RESTAPI_URL/api/v1/cati/instruments"} 
-                      else {"$env:ENV_RESTAPI_URL/api/v1/cati/serverparks/$($env:ServerParkName)/instruments"}
+try {
+    # If a serverpark is specified then limit the call to that server park
+    $catiInstrumentsUri = if([string]::IsNullOrEmpty($env:ServerParkName)) {"$env:ENV_RESTAPI_URL/api/v1/cati/instruments"} 
+                        else {"$env:ENV_RESTAPI_URL/api/v1/cati/serverparks/$($env:ServerParkName)/instruments"}
 
-# Retrieve a list of active instruments in CATI for a particular survey type I.E OPN
-$instruments = Invoke-RestMethod -Method Get -Uri $catiInstrumentsUri | where { $_.DeliverData -eq $true -and $_.name.StartsWith($env:SurveyType) }
+    # Retrieve a list of active instruments in CATI for a particular survey type I.E OPN
+    $instruments = Invoke-RestMethod -Method Get -Uri $catiInstrumentsUri | Where-Object { $_.DeliverData -eq $true -and $_.name.StartsWith($env:SurveyType) }
 
-# No active instruments found in CATI
-If ($instruments.Count -eq 0) {
-    Write-Host "No active instruments found for delivery"
+    # No active instruments found in CATI
+    If ($instruments.Count -eq 0) {
+        Write-Host "No active instruments found for delivery"
+    }
+
+    # Deliver the instrument package with data for each active instrument
+    foreach ($instrument in $instruments)
+    {
+        # Build uri to retrive instrument package file with data
+        $InstrumentDataUri = "$($env:ENV_RESTAPI_URL)/api/v1/serverparks/$($instrument.serverParkName)/instruments/$($instrument.name)/data"
+        
+        # Build data delivery filename for the instrument
+        $currentDateTime = (Get-Date)
+        $fileName = "$($instrument.name).$env:PackageExtension"
+
+        # Download instrument packagegit pu
+        Invoke-WebRequest $InstrumentDataUri -outfile $fileName 
+        Write-Host "Downloaded instrument '$($fileName)'"
+
+        # Generate and add SPSS files
+        & .\scripts\AddSpssFilesToInstrument.ps1 "$($instrument.name)" | Out-Null
+        Write-Host "Added SPSS files to instrument"
+
+        # Generate DD filename
+        $dataDeliveryFileName = "dd_$($instrument.name)_$($currentDateTime.ToString("ddMMyyyy"))_$($currentDateTime.ToString("HHmmss")).$env:PackageExtension";
+        Rename-Item -Path $fileName -NewName $dataDeliveryFileName
+        Write-Host "Renamed instrument to '$($dataDeliveryFileName)'"
+
+        # Upload instrument package to NIFI
+        gsutil cp $dataDeliveryFileName gs://$env:ENV_BLAISE_NIFI_BUCKET
+        Write-Host "Pushed instrument '$($dataDeliveryFileName)' to the NIFI bucket"
+        
+        # remove local instrument package
+        Remove-Item $dataDeliveryFileName
+    }
 }
-
-# Deliver the instrument package with data for each active instrument
-foreach ($instrument in $instruments)
-{
-    # Build uri to retrive instrument package file with data
-    $InstrumentDataUri = "$($env:ENV_RESTAPI_URL)/api/v1/serverparks/$($instrument.serverParkName)/instruments/$($instrument.name)/data"
-    
-    # Build data delivery filename for the instrument
-    $currentDateTime = (Get-Date)
-    $fileName = "$($instrument.name).$env:PackageExtension"
-
-    # Download instrument packagegit pu
-    wget $InstrumentDataUri -outfile $fileName 
-    Write-Host "Downloaded instrument '$($fileName)'"
-
-    # Generate and add SPSS files
-    & .\scripts\AddSpssFilesToInstrument.ps1 "$($instrument.name)"
-    Write-Host "Added SPSS files to instrument"
-
-    # Generate DD filename
-    $dataDeliveryFileName = "dd_$($instrument.name)_$($currentDateTime.ToString("ddMMyyyy"))_$($currentDateTime.ToString("HHmmss")).$env:PackageExtension";
-    Rename-Item -Path $fileName -NewName $dataDeliveryFileName
-    Write-Host "Renamed instrument to '$($dataDeliveryFileName)'"
-
-    # Upload instrument package to NIFI
-    gsutil cp $dataDeliveryFileName gs://$env:ENV_BLAISE_NIFI_BUCKET
-    Write-Host "Pushed instrument '$($dataDeliveryFileName)' to the NIFI bucket"
-    
-    # remove local instrument package
-    Remove-Item $dataDeliveryFileName
+catch{
+    Write-Host $_.ScriptStackTrace
+    exit 1
 }
