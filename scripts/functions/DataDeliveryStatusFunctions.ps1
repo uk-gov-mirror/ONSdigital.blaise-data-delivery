9082b698c106afd14634d41de5a06750862a2fbf
Sam Bryant <sam.bryant@armakuni.com>
2021-04-11 22:29:27 +0100

    Add id token to dds requests

--- scripts/functions/DataDeliveryStatusFunctions.ps1
+++ scripts/functions/DataDeliveryStatusFunctions.ps1
@@ -1,93 +1,102 @@
 . "$PSScriptRoot\LoggingFunctions.ps1"
 
-function CreateDataDeliveryStatus{
-        param(
-            [string] $fileName,
-            [string] $batchStamp,
-            [string] $state
-        )
-    try
-    {
+function GetIDToken {
+    return Invoke-RestMethod -UseBasicParsing "http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=$env:ENV_DDS_CLIENT&format=full" -Headers @{'Metadata-Flavor' = 'Google' }
+}
+
+function CreateDataDeliveryStatus {
+    param(
+        [string] $fileName,
+        [string] $batchStamp,
+        [string] $state
+    )
+    try {
         If ([string]::IsNullOrEmpty($fileName)) {
-            throw [System.IO.ArgumentException] "No file name name provided" }
-    
+            throw [System.IO.ArgumentException] "No file name name provided"
+        }
+
         If ([string]::IsNullOrEmpty($batchStamp)) {
-            throw [System.IO.ArgumentException] "No batch stamp name provided" }
-    
+            throw [System.IO.ArgumentException] "No batch stamp name provided"
+        }
+
         If ([string]::IsNullOrEmpty($state)) {
-            throw [System.IO.ArgumentException] "No state name provided" }
+            throw [System.IO.ArgumentException] "No state name provided"
+        }
 
         $DDSBaseUrl = "$env:ENV_DDS_URL/v1/state"
         $JsonObject = [ordered]@{
-                    state = "$($state)"
-                    batch = "$($batchStamp)"
-                }
+            state = "$($state)"
+            batch = "$($batchStamp)"
+        }
 
         $url = "$DDSBaseUrl/$fileName"
         $body = $JsonObject | ConvertTo-Json
-        Invoke-RestMethod -UseBasicParsing $url -ContentType "application/json" -Method POST -Body $body
+        $idToken = GetIDToken
+        Invoke-RestMethod -UseBasicParsing $url -ContentType "application/json" -Method POST -Body $body -Headers @{ 'Authorization' = "Bearer $idToken" }
     }
-    catch
-    {
+    catch {
         LogError("Creating Data Delivery Status failed: $($_.ScriptStackTrace)")
     }
 }
 
-function UpdateDataDeliveryStatus{
-        param(
-            [string] $fileName,
-            [string] $state
-        )
-    try
-    {
+function UpdateDataDeliveryStatus {
+    param(
+        [string] $fileName,
+        [string] $state
+    )
+    try {
         If ([string]::IsNullOrEmpty($fileName)) {
-            throw [System.IO.ArgumentException] "No file name name provided" }
-    
+            throw [System.IO.ArgumentException] "No file name name provided"
+        }
+
         If ([string]::IsNullOrEmpty($state)) {
-            throw [System.IO.ArgumentException] "No state name provided" }
+            throw [System.IO.ArgumentException] "No state name provided"
+        }
 
         $DDSBaseUrl = "$env:ENV_DDS_URL/v1/state"
         $JsonObject = [ordered]@{
-                    state = "$($state)"
-                }
+            state = "$($state)"
+        }
         $url = "$DDSBaseUrl/$fileName"
         $body = $JsonObject | ConvertTo-Json
-        Invoke-RestMethod -UseBasicParsing $url -ContentType "application/json" -Method PATCH -Body $body
+        $idToken = GetIDToken
+        Invoke-RestMethod -UseBasicParsing $url -ContentType "application/json" -Method PATCH -Body $body -Headers @{ 'Authorization' = "Bearer $idToken" }
     }
-    catch
-    {
+    catch {
         LogError("Updated Data Delivery Status failed: $($_.ScriptStackTrace)")
     }
 }
 
-function ErrorDataDeliveryStatus{
-        param(
-            [string] $fileName,
-            [string] $state,
-            [string] $error_info
-        )
-    try
-    {
+function ErrorDataDeliveryStatus {
+    param(
+        [string] $fileName,
+        [string] $state,
+        [string] $error_info
+    )
+    try {
         If ([string]::IsNullOrEmpty($fileName)) {
-            throw [System.IO.ArgumentException] "No file name name provided" }
-    
+            throw [System.IO.ArgumentException] "No file name name provided"
+        }
+
         If ([string]::IsNullOrEmpty($state)) {
-            throw [System.IO.ArgumentException] "No state name provided" }
+            throw [System.IO.ArgumentException] "No state name provided"
+        }
 
         If ([string]::IsNullOrEmpty($batchStamp)) {
-            throw [System.IO.ArgumentException] "No batch stamp name provided" }
+            throw [System.IO.ArgumentException] "No batch stamp name provided"
+        }
 
         $DDSBaseUrl = "$env:ENV_DDS_URL/v1/state"
         $JsonObject = [ordered]@{
-                    state = "$($state)"
-                    error_info = "$($error_info)"
-                }
+            state      = "$($state)"
+            error_info = "$($error_info)"
+        }
         $url = "$DDSBaseUrl/$fileName"
         $body = $JsonObject | ConvertTo-Json
-        Invoke-RestMethod -UseBasicParsing $url -ContentType "application/json" -Method PATCH -Body $body
+        $idToken = GetIDToken
+        Invoke-RestMethod -UseBasicParsing $url -ContentType "application/json" -Method PATCH -Body $body -Headers @{ 'Authorization' = "Bearer $idToken" }
     }
-    catch
-    {
+    catch {
         LogError("Sending Error message to Data Delivery Status failed: $($_.ScriptStackTrace)")
     }
 }
