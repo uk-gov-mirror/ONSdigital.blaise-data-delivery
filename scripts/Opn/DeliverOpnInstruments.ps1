a5e3303646daa0dff9c5f2d75eec796eb959ab0b
Nik Webb <nik.webb@ons.gov.uk>
2021-05-06 12:08:45 +0100

    check what version of powershell az is using

--- scripts/Opn/DeliverOpnInstruments.ps1
+++ scripts/Opn/DeliverOpnInstruments.ps1
@@ -11,6 +11,7 @@
 . "$PSScriptRoot\..\functions\ManipulaFunctions.ps1"
 
 try {
+    $PSVersionTable
     # Retrieve a list of active instruments in CATI for a particular survey type I.E OPN
     $instruments = GetListOfInstrumentsBySurveyType
 
