7dc8a2a1eaecc30ada4ec81d88f52814e23bdbe6
jamie kerr <jamiestephenkerr@gmail.com>
2023-06-30 16:04:20 +0100

    Updated config jsons to include flag for audit trail data and added the flag to the cli command call

--- scripts/DeliverQuestionnaireData.ps1
+++ scripts/DeliverQuestionnaireData.ps1
@@ -87,7 +87,7 @@ try {
             DownloadFileFromBucket -questionnaireFileName "$($_.name).bpkg" -bucketName $using:dqsBucket -filePath $deliveryFile
 
             # Populate data
-            C:\BlaiseServices\BlaiseCli\blaise.cli datadelivery -s $using:serverParkName -q $_.name -f $deliveryFile
+            C:\BlaiseServices\BlaiseCli\blaise.cli datadelivery -s $using:serverParkName -q $_.name -f $deliveryFile -a $using:config.auditTrailData
 
             # Create a temporary folder for processing questionnaires
             $processingFolder = CreateANewFolder -folderPath $using:tempPath -folderName "$($_.name)_$(Get-Date -format "ddMMyyyy")_$(Get-Date -format "HHmmss")"
